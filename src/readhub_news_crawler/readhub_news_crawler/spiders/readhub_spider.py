4163439d0ca2dfc0b3db234141ac48afe3f56a7d
chen hao <haoeric@hotmail.com>
2019-01-23 01:11:01 +0800

    update readhub scrapy to fit for css change

--- src/readhub_news_crawler/readhub_news_crawler/spiders/readhub_spider.py
+++ src/readhub_news_crawler/readhub_news_crawler/spiders/readhub_spider.py
@@ -10,21 +10,23 @@ class newsItem(scrapy.Item):
     
 class readhubSpider(scrapy.Spider):
     name = "readhub"
-    start_urls = ['https://readhub.me']
+    start_urls = ['https://readhub.cn']
 
     def parse(self, response):
         item = newsItem()
         all_news = response.xpath('//*[@id="itemList"]/div')
+        print("Total News crawlered: " + str(len(all_news)))
         for news in all_news:
             date = time.strftime("%d-%m-%Y")
-            title = news.css('div.topicItem___3YVLI h2.topicTitle___1M353 span.content___2vSS6::text').extract()
-            links = news.css('div.articleItem___2P-7U a.articleTitle___3zy5I::attr(href)').extract()
-            
-            item['id'] = date + "-" + str(hash(title[0]))
-            item['date'] = date
-            item['title'] = title
-            item['content'] = news.css('div.summary___1i4y3 div.bp-pure___3xB_W::text').extract()
-            item['source'] = ";".join(links)
-            yield item
+            title = news.css('div.topicItem___1B0j1 h2.topicTitle___1HWIA a.content___3EhkM::text').extract()
+            print(title)
+            if len(title) > 0:
+                links = news.css('div.articleItem___2P-7U a.articleTitle___3zy5I::attr(href)').extract()
+                item['id'] = date + "-" + str(hash(title[0]))
+                item['date'] = date
+                item['title'] = title
+                item['content'] = news.css('div.summary___3jny8 div.bp-pure___3xB_W::text').extract()
+                item['source'] = ";".join(links)
+                yield item
             
         
--- src/test.sh
+++ src/test.sh
@@ -1,2 +1,7 @@
+## test scrapy crawler
+scrapy crawl readhub -t csv -o /Users/chenhao/Downloads/readhub_news.csv --loglevel=INFO
+
+
+
 ## test docker, generate results locally
 docker run -t -v /Users/chenhao/Downloads:/out_data -w=/ -t daily_digest_alexa --bucket_key haoeric-daily-digest-news-audio --bucket_path fresh-news --working_dir /out_data
